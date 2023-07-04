bb52290c5593f22e4e40ee4a98fae94207d6c425
CCranney <cwcranney@proton.me>
2023-07-04 11:33:01 -0700

    update for github pages

---
+++ src/components/dendrogram/DendrogramChart.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import * as d3 from "d3";
+
+export const DendrogramChart = () => {
+
+    const [data, setData] = useState([]);
+    const [isLoading, setLoading] = useState(true);
+
+    useEffect(() => {
+        d3.csv('/data/dendrogram/treeoflife_links.csv').then((d) => {
+            console.log(d);
+            const hierarchyGenerator = d3.stratify()
+                .id((node) => node.target_node_id)
+                .parentId((node) => node.source_node_id);
+            const hierarchy = hierarchyGenerator(d);
+            console.log(hierarchy);
+
+            setData(data);
+            setLoading(false);
+        });
+    }, [])
+
+
+
+    return (
+        <div>DendrogramChart</div>
+    )
+}
